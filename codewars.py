acb6701b4684793ca566ef5737589e68433530bf
Daniel Juarez <juarezdaniel@gmail.com>
2022-02-26 10:13:09 -0500

    4 python kadas

--- codewars.py
+++ codewars.py
@@ -33,3 +33,43 @@ def sum_str(a, b):
         second_value = int(b)
     
     return str(first_value + second_value)
+
+
+# We need a function that can transform a number into a string.
+
+# What ways of achieving this do you know?
+
+def number_to_string(num):
+    return str(num)
+
+
+#Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+def digital_root(n):
+    x = sum(int(i) for i in str(n))
+    if x > 9:
+       return digital_root(x)
+    return x
+
+#Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+#For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.'
+
+def sum_two_smallest_numbers(numbers):
+    numbers.sort()
+    return numbers[0] + numbers[1]
+
+# Return the number (count) of vowels in the given string.
+
+# We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+# The input string will only consist of lower case letters and/or spaces.
+
+def get_count(sentence):
+    vowel_count = 0
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    for i in sentence:
+        if i in vowels:
+            vowel_count = vowel_count + 1
+    return vowel_count
+
